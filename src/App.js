c70d7c1821830461b98783145015259c58b2087a
lap00020 <lap00020@mix.wvu.edu>
2024-02-27 13:13:53 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './Navbar.js';
+import './Card.js';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800397955</p>
       <p>Hi I am Logan</p>
+      <Card />
     </div>
   );
 }
